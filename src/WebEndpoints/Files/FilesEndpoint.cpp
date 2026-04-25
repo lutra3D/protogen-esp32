ac0e70962a0b2aa019a99758b1ea5a1072a68b91
Milan Mikuš <mylan719@gmail.com>
2026-04-25 20:21:03 +0200

    Add GET /files/info storage usage endpoint (#36)

--- src/WebEndpoints/Files/FilesEndpoint.cpp
+++ src/WebEndpoints/Files/FilesEndpoint.cpp
@@ -11,6 +11,8 @@ void FilesEndpoint::registerEndpoint(AsyncWebServer &server)
 {
   server.on("/files", HTTP_GET, [this](AsyncWebServerRequest *request)
             { handleGet(request); });
+  server.on("/files/info", HTTP_GET, [this](AsyncWebServerRequest *request)
+            { handleGetInfo(request); });
 }
 
 void FilesEndpoint::handleGet(AsyncWebServerRequest *request)
@@ -28,6 +30,30 @@ void FilesEndpoint::handleGet(AsyncWebServerRequest *request)
     JsonObject fileObject = files.add<JsonObject>();
     file.serialize(fileObject);
   }
+
+  String json;
+  serializeJson(document, json);
+
+  request->send(200, "application/json", json);
+}
+
+void FilesEndpoint::handleGetInfo(AsyncWebServerRequest *request)
+{
+  const size_t totalSizeBytes = fileManager_.totalBytes();
+  const size_t usedSizeBytes = fileManager_.usedBytes();
+
+  float usedPercentage = 0.0F;
+  if (totalSizeBytes > 0U)
+  {
+    usedPercentage = (static_cast<float>(usedSizeBytes) * 100.0F) /
+                     static_cast<float>(totalSizeBytes);
+  }
+
+  JsonDocument document;
+  JsonObject info = document.to<JsonObject>();
+  info["totalSizeBytes"] = totalSizeBytes;
+  info["usedSizeBytes"] = usedSizeBytes;
+  info["usedPercentage"] = usedPercentage;
 
   String json;
   serializeJson(document, json);
