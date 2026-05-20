034e0babdf7979bea6a66eb79443455699ab4844
Mylan719 <Mylan719@gmail.com>
2026-05-20 20:07:58 +0200

    fixed face display orientation

--- src/FaceDisplay/NeopixelFaceDisplay.cpp
+++ src/FaceDisplay/NeopixelFaceDisplay.cpp
@@ -71,10 +71,13 @@ void NeopixelFaceDisplay::drawPixel(int x, int y, Color color)
     return;
   }
 
-  const uint16_t rightX = panelX - panelWidth_;
+  //right display is mirrored horizontally and vertically relative to the left display, so we need to transform the coordinates accordingly
+  const uint16_t rightX = 2*panelWidth_ - panelX-1;
+  const uint16_t rightY = panelHeight_ - panelY-1;
+
   if (rightX < panelWidth_)
   {
-    rightPanel_.setPixelColor(getPixelIndex(rightX, panelY), pixelColor);
+    rightPanel_.setPixelColor(getPixelIndex(rightX, rightY), pixelColor);
   }
 }
 
@@ -103,12 +106,33 @@ void NeopixelFaceDisplay::afterFrameRendered()
 
 uint16_t NeopixelFaceDisplay::getPixelIndex(uint16_t x, uint16_t y) const
 {
-  const uint16_t rowOffset = y * panelWidth_;
+  // any out of bounds address maps to the first hidden pixel
+    if ((x >= panelWidth_) || (y >= panelHeight_))
+    {
+        return 0;
+    }
 
-  if ((y & 1U) == 0)
-  {
-    return rowOffset + x;
-  }
+    const uint8_t XYTable[] = {
+      240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
+      239, 238, 237, 236, 235, 234, 233, 232, 231, 230, 229, 228, 227, 226, 225, 224,
+      208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
+      207, 206, 205, 204, 203, 202, 201, 200, 199, 198, 197, 196, 195, 194, 193, 192,
+      176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
+      175, 174, 173, 172, 171, 170, 169, 168, 167, 166, 165, 164, 163, 162, 161, 160,
+      144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
+      143, 142, 141, 140, 139, 138, 137, 136, 135, 134, 133, 132, 131, 130, 129, 128,
+      112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
+      111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99,  98,  97,  96,
+      80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+      79,  78,  77,  76,  75,  74,  73,  72,  71,  70,  69,  68,  67,  66,  65,  64,
+      48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+      47,  46,  45,  44,  43,  42,  41,  40,  39,  38,  37,  36,  35,  34,  33,  32,
+      16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+      15,  14,  13,  12,  11,  10,  9,   8,   7,   6,   5,   4,   3,   2,   1,   0
+    };
 
-  return rowOffset + (panelWidth_ - 1U - x);
+    uint8_t i = (y * panelWidth_) + x;
+    uint8_t j = XYTable[i];
+
+    return j;
 }
